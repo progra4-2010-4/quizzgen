d7a8093e891131b78c49184ba9169873e1e56836
lfborjas <luis.borjas@escolarea.com>
2010-12-06 23:43:52 -0600

    ahora en sinatra

--- app/controllers/application_controller.rb
+++
@@ -1,4 +0,0 @@
-class ApplicationController < ActionController::Base
-  include Clearance::Authentication
-  protect_from_forgery
-end
--- config/environments/cucumber.rb
+++
@@ -1,1 +0,0 @@
-config.action_mailer.default_url_options = { :host => 'localhost:3000' }
